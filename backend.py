b775c6f5a384fe6b1943715547283e5f71bb8b62
Shawn Butler <117597766+ShawnButler2021@users.noreply.github.com>
2024-02-13 14:30:18 -0600

    Creating the basic Flask application
    I updated backend.py to contain the basic code for the Flask application.

--- backend.py
+++ backend.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template, request, redirect
 
+# Creating the application object
+web_app = Flask(__name__)
+
+
+@web_app.route("/")
+def home_page():
+  return render_template('home.html')
+
+
+
+@web_app.route("/cart")
+def cart_page():
+  return render_template('cart.html')
+
+
+@web_app.route("/order_history.html")
+def order_history_page():
+  return render_template('order_history.html')
+
+
+@web_app.route("/settings")
+def settings_page():
+  return render_template('settings.html')
